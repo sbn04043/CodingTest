8940510d64a5fbdee1fa60b287e3c600a7a62732
sbn04043 <70567694+sbn04043@users.noreply.github.com>
2024-06-22 14:52:33 +0900

    [level 1] Title: 폰켓몬, Time: 2.59 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        if (nums.length / 2 > set.size()) {
+            return set.size();
+        } else {
+            return nums.length / 2;
+        }
+        
+    }
+}
