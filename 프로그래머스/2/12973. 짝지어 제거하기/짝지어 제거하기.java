0944ed1b1faf69b8827f89c96cdd496e740d6879
sbn04043 <70567694+sbn04043@users.noreply.github.com>
2024-06-22 11:00:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 45.79 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = -1;
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty() || stack.peek() != c) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty()? 1 : 0;
+    }
+}
